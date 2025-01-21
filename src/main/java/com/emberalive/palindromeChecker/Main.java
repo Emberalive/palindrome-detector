260aa23bc7152696dd8f5051fa909b161e48362a
samuel <samueltsutcliffe@gmail.com>
2025-01-21 01:22:57 +0000

    added log4j dependecies, as well as used it in the Main and to the Controller

--- src/main/java/com/emberalive/palindromeChecker/Main.java
+++ src/main/java/com/emberalive/palindromeChecker/Main.java
@@ -1,14 +1,18 @@
 package com.emberalive.palindromeChecker;
 
-import com.emberalive.palindromeChecker.rest.Controller;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 @SpringBootApplication
 public class Main {
+    private static final Logger logger = LogManager.getLogger(Main.class);
     public static void main(String[] args) {
+        logger.info("Starting Palindrome Checker");
         SpringApplication.run(Main.class, args);
+        logger.info("Application started successfully");
     }
 }
--- src/main/java/com/emberalive/palindromeChecker/rest/Controller.java
+++ src/main/java/com/emberalive/palindromeChecker/rest/Controller.java
@@ -4,20 +4,25 @@ import org.springframework.core.io.ClassPathResource;
 import org.springframework.core.io.Resource;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 import java.util.HashMap;
 import java.util.Map;
 
 @RestController
 public class Controller {
+    private static final Logger logger = LogManager.getLogger(Controller.class);
 
     @GetMapping("/")
     public ResponseEntity<Resource> index() {
+        logger.info("Accessed the home page");
         Resource home = new ClassPathResource("templates/home.html");
         return ResponseEntity.ok().body(home);
     }
     @PostMapping("/check")
     public ResponseEntity<Map<String, String>> CheckPallindrome(@RequestParam("palindrome")String palindrome) {
+        logger.debug("recieved input" + palindrome);
         String cleanedString = palindrome.toLowerCase().trim();
         String reversedString = new StringBuilder(cleanedString).reverse().toString();
         
@@ -27,9 +32,11 @@ public class Controller {
         if (palindrome.equals(reversedString)) {
             message = palindrome + "is a palindrome.";
             backgroundColor = "green";
+            logger.info("Input is a palindrome: {}", palindrome);
         } else {
             message = palindrome + " is not a palindrome.";
             backgroundColor = "red";
+            logger.info("Input is not a palindrome: {}", palindrome);
         }
 
         Map<String, String> response = new HashMap<>();
