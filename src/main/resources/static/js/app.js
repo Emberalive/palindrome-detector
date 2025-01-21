b85afcee00c6360c5879536c63ecfb438c20567e
samuel <samueltsutcliffe@gmail.com>
2025-01-21 00:34:22 +0000

    added a license

--- src/main/resources/static/js/app.js
+++ src/main/resources/static/js/app.js
@@ -1,3 +1,4 @@
+//adds an action listener to prevent the form submition from reloading the page
 document.addEventListener('DOMContentLoaded', function(){
     const form = document.querySelector("form");
     const boxDiv = document.querySelector(".box")
@@ -16,11 +17,13 @@ document.addEventListener('DOMContentLoaded', function(){
         })
             .then(response => response.json())
             .then(data => {
+                //finding the element id with result
                 const resultElement = document.querySelector('#result');
                 if (data && data.message){
+                    //setting the value of message
                     resultElement.innerText = data.message;
                     boxDiv.classList.add(data.backgroundColor); // Add the color class (green or red)
-                    boxDiv.classList.remove(data.backgroundColor === "green" ? "red" : "green");
+                    boxDiv.classList.remove(data.backgroundColor === "green" ? "red" : "green"); //changes the classlist of .box class depending on what the value of the data.background variable holds
                 } else {
 
                 }
